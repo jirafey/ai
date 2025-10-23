751b7cc56f6d2beca861454eaa764ecf16e4a02c
jirafey <jirafey>
2025-10-23 21:02:11 +0200

    Add pointer, fix edit, add search not working yet

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const input = document.getElementById('taskInput');
 const list = document.getElementById('myUL');
 const addButton = document.getElementById('addBtn');
+const searchButton = document.getElementById('searchInput');
+const search = document.getElementById('searchInput');
 
 class Todo {
     constructor() {
         const savedTasks = JSON.parse(localStorage.getItem('user'));
         this.tasks = savedTasks || [];
-        this.draw();
+        this.draw(this.tasks);
         this.delegate();
     }
 
-    draw() {
+    draw(array) {
         list.innerHTML = '';
-        for (const [index, task] of this.tasks.entries()){
+        for (const [index, task] of array.entries()) {
             const li = document.createElement('li');
             li.dataset.index = index;
 
@@ -21,7 +23,7 @@ class Todo {
 
             const deleteNode = document.createElement('span');
             deleteNode.className = 'delete';
-            deleteNode.innerHTML ='❌';
+            deleteNode.innerHTML = '❌';
             li.appendChild(deleteNode);
             li.appendChild(document.createTextNode(' '));
 
@@ -29,46 +31,68 @@ class Todo {
             editNode.className = 'edit';
             editNode.innerHTML = '✏️';
             li.appendChild(editNode);
-            
+
             list.appendChild(li);
         }
     }
+    save(){
+        localStorage.setItem('user', JSON.stringify(this.tasks));
+    }
     add() {
         const taskText = input.value.trim();
-        if (taskText) {
+        if (taskText.length >= 3 && taskText.length <= 255) {
             this.tasks.push(taskText);
-            localStorage.setItem('user', JSON.stringify(this.tasks));
-            this.draw();
+            this.save();
+            this.draw(this.tasks);
         }
 
         input.value = '';
     }
     del(index) {
         this.tasks.splice(index, 1);
-        this.draw();
+        this.save();
+        this.draw(this.tasks);
     }
     edit(index, x) {
-        this.tasks[index]=x;
-        this.draw();
+        this.tasks[index] = x;
+        this.save();
+        this.draw(this.tasks);
     }
-    delegate(){
+    filterItems(array, query){
+        return array.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+    }
+    search(searchTerm) {
+        const tempTasks = this.filterItems(this.tasks, searchTerm);
+        this.draw(tempTasks);
+        if (!searchTerm) {
+            this.draw(this.tasks);
+        }
+    }
+    delegate() {
         const t = document.getElementById('myUL');
 
-        t.onclick = (event) =>  {
+        t.onclick = (event) => {
             const clickedElement = event.target;
             const listItem = clickedElement.closest('li');
-            
+            if (!listItem) return; 
+            const index = listItem.dataset.index;
+
             if (clickedElement.classList.contains('delete')) {
-                const index = listItem.dataset.index;
                 this.del(index);
             }
-            else if(clickedElement.classList.contains('edit')) {
-                const index = listItem.dataset.index;
-                const editPrompt = prompt("change name of the task");
-                this.edit(index, editPrompt);
-            }
             else  {
-                return;
+                const oldText = this.tasks[index];
+                let newText = prompt("Change name of the task", oldText);
+                if (newText === null){
+                    return;
+                }
+                newText = newText.trim();
+
+                if (newText.length < 3 || newText.length > 255) {
+                    alert("Task has to be between 3 and 255 characters");
+                    return;
+                }
+                this.edit(index, newText);
             }
         }
     }
@@ -78,4 +102,7 @@ const myTodo = new Todo();
 addButton.addEventListener("click", () => {
     myTodo.add();
 });
-
+searchButton.addEventListener("input", (event) => {
+    const searchTerm = event.target.value;
+    myTodo.search(searchTerm);
+});
